ba2a6e4376378c2d4f4432df2318b085a48c9bf9
Gabriel de Almeida Sousa <110002760+Almeida-S-Gabriel@users.noreply.github.com>
2025-06-12 10:08:35 -0300

    Update MainActivity.kt

--- TasksManagerApp/app/src/main/java/com/example/tasksmanagerapp/MainActivity.kt
+++ TasksManagerApp/app/src/main/java/com/example/tasksmanagerapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tasksmanagerapp.Screens.TaskScreen
 import com.example.tasksmanagerapp.ui.theme.TasksManagerAppTheme
 
 class MainActivity : ComponentActivity() {
